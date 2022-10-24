fe917f38b9dd7cd4687447f6a66e0a234fb03373
federicopotrich <federico.potrich@marconirovereto.it>
2022-10-24 15:19:33 +0200

    gestione iniziale dei piani

--- LanParty/Assets/c# scripts/PlayerController.cs
+++ LanParty/Assets/c# scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    int piano = 0;
+    public GameObject [] piani;
     public float speed;
     // Start is called before the first frame update
     void Start()
@@ -18,5 +20,28 @@ public class PlayerController : MonoBehaviour
         float y = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(x, y, 0);
         transform.Translate(movement * speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D collisionDetected){
+        //Debug.Log();
+        if(collisionDetected.gameObject.tag == "scale"){
+            piani[piano].SetActive(false);
+            if(collisionDetected.gameObject.name=="up"){
+                piano++;
+                piani[piano+1].SetActive(true);
+            }else if(collisionDetected.gameObject.name=="up"){
+                piano--;
+                piani[piano-1].SetActive(true);
+            }
+            //pianoTerra.SetActive(false);
+            //piano1.SetActive(true);
+
+
+            if(collisionDetected.gameObject.transform.parent.gameObject.tag == "sinistra")
+                this.transform.position = new Vector3(this.transform.position.x+5, this.transform.position.y, 0);
+            else
+                this.transform.position = new Vector3(this.transform.position.x-5, this.transform.position.y, 0);
+
+        }
     }
 }
