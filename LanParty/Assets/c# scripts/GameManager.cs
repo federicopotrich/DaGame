4870434c2d0fb4fc87d95f0447f7720626a372c8
federicopotrich <federico.potrich@marconirovereto.it>
2022-11-20 21:15:50 +0100

    aggiunta tutta la lista delle armi nello shop

--- LanParty/Assets/c# scripts/GameManager.cs
+++ LanParty/Assets/c# scripts/GameManager.cs
@@ -6,9 +6,39 @@ public class GameManager : MonoBehaviour
 {
     public WeponsManager [] armi;
     public ArmorsManager [] armature;
+
+    public GameObject container;
+    public GameObject slotShop;
     void Start()
     {
-        
+        for (int i = 0; i < armi.Length; i++)
+        {
+            GameObject gInstantiated = GameObject.Instantiate(slotShop, new Vector3(), Quaternion.identity);
+            gInstantiated.transform.Find("");
+            gInstantiated.transform.SetParent(container.transform);
+            gInstantiated.transform.localScale = new Vector3(1, 1, 1);
+
+            gInstantiated.transform.Find("Image").GetComponent<UnityEngine.UI.Image>().sprite = armi[i].imageWeapon;
+            gInstantiated.transform.Find("TextNome").GetComponent<TMPro.TextMeshProUGUI>().text = armi[i].nome;
+            gInstantiated.transform.Find("TextDmg").GetComponent<TMPro.TextMeshProUGUI>().text = ""+armi[i].dmg;
+            gInstantiated.transform.Find("TextCost").GetComponent<TMPro.TextMeshProUGUI>().text = ""+armi[i].cost;
+            gInstantiated.transform.Find("TextRarita").GetComponent<TMPro.TextMeshProUGUI>().text = armi[i].rarita;
+        }
+
+        for (int i = 0; i < armature.Length; i++)
+        {
+            GameObject gInstantiated = GameObject.Instantiate(slotShop, new Vector3(), Quaternion.identity);
+            gInstantiated.transform.SetParent(container.transform);
+            gInstantiated.transform.localScale = new Vector3(1, 1, 1);
+
+            
+
+            gInstantiated.transform.Find("Image").GetComponent<UnityEngine.UI.Image>().sprite = armature[i].imageArmor;
+            gInstantiated.transform.Find("TextNome").GetComponent<TMPro.TextMeshProUGUI>().text = armature[i].nome;
+            gInstantiated.transform.Find("TextDmg").GetComponent<TMPro.TextMeshProUGUI>().text = ""+armature[i].defence;
+            gInstantiated.transform.Find("TextCost").GetComponent<TMPro.TextMeshProUGUI>().text = ""+armature[i].cost;
+            gInstantiated.transform.Find("TextRarita").GetComponent<TMPro.TextMeshProUGUI>().text = armature[i].rarita;
+        }
     }
 
     // Update is called once per frame
