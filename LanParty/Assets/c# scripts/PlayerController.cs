ffaa13ef2afce1ae67d4f2cf1a2b3ef05f004b14
federicopotrich <federico.potrich@marconirovereto.it>
2023-01-02 15:37:40 +0100

    fix del movimento

--- LanParty/Assets/c# scripts/PlayerController.cs
+++ LanParty/Assets/c# scripts/PlayerController.cs
@@ -23,8 +23,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        float x = 0;
+        float y = 0;
+        if(Input.GetKey(KeyCode.A) != Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.LeftArrow) != Input.GetKey(KeyCode.RightArrow)){
+            x = Input.GetAxis("Horizontal");
+        }
+        if(Input.GetKey(KeyCode.W) != Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.UpArrow) != Input.GetKey(KeyCode.DownArrow)){
+            y = Input.GetAxis("Vertical");
+        }
         
         this.GetComponent<Animator>().SetFloat("speed", Mathf.Abs(x));
 
