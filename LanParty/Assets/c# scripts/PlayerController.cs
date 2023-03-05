a0a9a5e428f478216c607bb8ee4a15ec7f628923
federicopotrich <federico.potrich@marconirovereto.it>
2023-03-05 11:57:13 +0100

    modifiche sulla gestione dei piani tramite spawnpoint

--- LanParty/Assets/c# scripts/PlayerController.cs
+++ LanParty/Assets/c# scripts/PlayerController.cs
@@ -119,19 +119,22 @@ public class PlayerController : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collisionDetected){
         if(collisionDetected.gameObject.tag == "scale"){
             piani[piano].SetActive(false);
-
+            
             if(collisionDetected.gameObject.name=="up"){
                 piani[piano+1].SetActive(true);
+                //this.transform.position = collisionDetected.gameObject.transform.Find("SpawnPoint").transform.position;
+                this.transform.position = piani[piano+1].gameObject.transform.Find(collisionDetected.gameObject.transform.parent.gameObject.name).transform.Find("down").transform.Find("SpawnPoint").transform.position;
                 piano++;
             }else if(collisionDetected.gameObject.name=="down"){
                 piani[piano-1].SetActive(true);
+                if(piano-1 == 0){
+                    this.transform.position = piani[piano-1].gameObject.transform.Find(collisionDetected.gameObject.transform.parent.gameObject.name).transform.Find("up").transform.Find("SpawnPoint").transform.position;
+                }else{
+                    this.transform.position = piani[piano-1].gameObject.transform.Find(collisionDetected.gameObject.transform.parent.gameObject.name).transform.Find("down").transform.Find("SpawnPoint").transform.position;
+                }
                 piano--;
             }
 
-            if(collisionDetected.gameObject.transform.parent.gameObject.tag == "sinistra")
-                this.transform.position = new Vector3(collisionDetected.gameObject.transform.position.x+10, collisionDetected.gameObject.transform.position.y, 0);
-            else
-                this.transform.position = new Vector3(collisionDetected.gameObject.transform.position.x-10, collisionDetected.gameObject.transform.position.y, 0);
 
         }
         
