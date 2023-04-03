89621388c7a360fe1cbb30e7418271d776b5675c
andrea.capobianco <andrea.capobianco@marconirovereto.it>
2023-04-03 15:26:35 +0200

    matematica rifatta

--- LanParty/Assets/c# scripts/MatematicaGameManager.cs
+++ LanParty/Assets/c# scripts/MatematicaGameManager.cs
@@ -7,57 +7,64 @@ public class MatematicaGameManager : MonoBehaviour
     public ArrayList matrix;
     public int[] row;
 
+    public int[,] bigTable = new int[5,5];
     public GameObject [] cells;
 
     public Sprite [] numbers;
     // Start is called before the first frame update
     void Start()
     {
-
-        matrix = new ArrayList(5);
-        
-            row = new int[]{1, 1, 1, 1, 1};
-            matrix.Add(row);
-            row = new int[]{2, 2, 2, 2, 2};
-            matrix.Add(row);
-            row = new int[]{3, 3, 3, 3, 3};
-            matrix.Add(row);
-            row = new int[]{4, 4, 4, 4, 4};
-            matrix.Add(row);
-            row = new int[]{5, 5, 5, 5, 5};
-            matrix.Add(row);
-
-            for(int j = 0; j < 5000; j++){       
-                int col = Random.Range(0, 5);
-                int row = Random.Range(0, 5);
-                int col2 = Random.Range(0, 5);
-                int row2 = Random.Range(0, 5);
-
-                int tmp = ((int[]) matrix[col]) [row];
-                ((int[]) matrix[col]) [row] = ((int[]) matrix[col2]) [row2];
-                ((int[]) matrix[col2]) [row2] = tmp;
-            }
+        int [,] matrix = nextBigTable();
             
+        
+        
+    }
+
+    private int[,] nextBigTable() {
+        //preparo la tabella con i valori ordinati
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                bigTable[i,j] = i+1;
+            }
+        }
+        
+        //mescolo la tabella
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                int tmpI = Random.Range(0, 4);
+                int tmpJ = Random.Range(0, 4);
+                int tmp = bigTable[i,j];
+                bigTable[i,j] = bigTable[tmpI,tmpJ];
+                bigTable[tmpI,tmpJ] = tmp;
+            }
+        }
         
         int k = 0;
         for(int i = 0; i < 5; i++){
             for(int j = 0; j < 5; j++){       
                 cells[k].AddComponent<cell>();
-                cells[k].GetComponent<cell>().value = ((int[])(matrix[i]))[j];
+                cells[k].GetComponent<cell>().value = bigTable[i,j];
                 cells[k].GetComponent<cell>().row = j;
                 cells[k].GetComponent<cell>().col = i;
-                cells[k].GetComponent<UnityEngine.UI.Image>().sprite = numbers[((int[])(matrix[i]))[j]-1];
+                cells[k].GetComponent<UnityEngine.UI.Image>().sprite = numbers[bigTable[i, j]-1];
                 k++;
             }
         }
+        return bigTable;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    public void eventMouseCellListener(){
         
     }
 }
+
+
 public class cell : MonoBehaviour
 {
     public int col;
