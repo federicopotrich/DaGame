7c1f606a74c0212f87d89e5b8fea2cf1a71eafde
cristian-zeni-marconi <cristian.zeni@marconirovereto.it>
2023-04-03 16:04:05 +0200

    started attacks

--- LanParty/Assets/c# scripts/Bossfight/attacks.cs
+++ LanParty/Assets/c# scripts/Bossfight/attacks.cs
@@ -2,9 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class attacks : MonoBehaviour
 {
     
-    
+    // attacks = [laser, spin, punch, yeet, ira_degli_dei, stomp]
+
+    public void call(int i){
+        switch (i)
+        {
+            case 0:  break;
+            case 1:  break;
+            case 2:  break;
+            case 3:  break;
+            case 4:  break;
+            case 5:  break;
+
+            default:
+        }
+    }
 
 }
