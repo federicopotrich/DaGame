9087080c181787b3ef4e2180c638c3d6d29411df
alexanderpardatscher <alexander.pardatscher@marconirovereto.it>
2023-04-03 15:25:27 +0200

    ANTENNORG IS MOVINGgit add .git add .

--- LanParty/Assets/c# scripts/Bossfight/bossController.cs
+++ LanParty/Assets/c# scripts/Bossfight/bossController.cs
@@ -1,21 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.IO;
 public class bossController : MonoBehaviour
 {
     
     public int hp;
+    public Transform[] BossSpots;
+    float speed;
+    int spotIndex;
+    bool b;
 
     void Start()
     {
-        
+        speed = 10f;
+        spotIndex = (int) Random.Range(0,3);
+        b = false;
     }
 
     void Update()
     {
-        
-        
+        if(!b)
+            Move();   
+        else
+            ChangeDirection();
+    }
 
+    void Move(){
+
+        this.transform.position = Vector2.MoveTowards(this.transform.position, BossSpots[spotIndex].transform.position, speed*Time.deltaTime);
+        if(this.transform.position==BossSpots[spotIndex].transform.position)
+            b=true;
+    }
+
+    void ChangeDirection(){
+        spotIndex = (int) Random.Range(0,3);
+        b = false;
     }
 }
