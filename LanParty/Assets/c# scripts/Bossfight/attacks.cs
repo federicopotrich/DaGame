c16b4af0e7b17ca506ed1824c414f2778ef38e83
alexanderpardatscher <alexander.pardatscher@marconirovereto.it>
2023-04-06 12:20:32 +0200

    'minor' changes to the boss beahaviour and attacks backend so the boss has random behaviour between moving and attacking

--- LanParty/Assets/c# scripts/Bossfight/attacks.cs
+++ LanParty/Assets/c# scripts/Bossfight/attacks.cs
@@ -2,23 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class attacks : MonoBehaviour
+public class Attacks : MonoBehaviour
 {
     
     // attacks = [laser, spin, punch, yeet, ira_degli_dei, stomp]
+    public Animator bossAnim;
 
-    public void call(int i){
+    public IEnumerator call(int i){
+        Debug.Log("Attacking");
         switch (i)
         {
-            case 0:  break;
-            case 1:  break;
-            case 2:  break;
-            case 3:  break;
-            case 4:  break;
-            case 5:  break;
+            case 0:  Debug.Log("Spin Attack!"); break;
+            case 1:  Debug.Log("Laser Attack!"); break;
+            case 2:  Debug.Log("Punch Attack!"); yield return StartCoroutine(Punch()); break;
+            case 3:  Debug.Log("Yeet Attack!"); break;
+            case 4:  Debug.Log("IRA DEGLI DEI! Attack!"); break;
+            case 5:  Debug.Log("Stomp Attack!"); break;
 
             default: break;
         }
+    }
+
+    IEnumerator Punch(){
+        Debug.Log("Punch!");
+        yield return new WaitForSeconds(1f);
+        bossAnim.SetBool("punch",true);
+        yield return new WaitForSeconds(0.2f);
+        bossAnim.SetBool("punch",false);
     }
 
 }
