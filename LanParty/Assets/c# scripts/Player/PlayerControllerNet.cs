e313006cbe09f505a9207b7aaa9e97589dab703b
federicopotrich <federico.potrich@marconirovereto.it>
2023-04-07 21:35:08 +0200

    inizio progettazione del gioco versione multiplayer

--- LanParty/Assets/c# scripts/Player/PlayerControllerNet.cs
+++ LanParty/Assets/c# scripts/Player/PlayerControllerNet.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Unity.Netcode;
 public class PlayerControllerNet : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
     void Start()
     {
         
@@ -13,6 +13,16 @@ public class PlayerControllerNet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float x = 0;
+        float y = 0;
+        if(Input.GetKey(KeyCode.A) != Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.LeftArrow) != Input.GetKey(KeyCode.RightArrow)){
+            x = Input.GetAxis("Horizontal");
+        }
+        if(Input.GetKey(KeyCode.W) != Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.UpArrow) != Input.GetKey(KeyCode.DownArrow)){
+            y = Input.GetAxis("Vertical");
+        }
+
+        this.gameObject.transform.Translate(new Vector3(x*Time.deltaTime*speed, y*Time.deltaTime*speed, 0));
+
     }
 }
