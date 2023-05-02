f79db0d271ef18ae8225e62dada11ee901f52707
federicopotrich <federico.potrich@marconirovereto.it>
2023-05-02 23:49:04 +0200

    aggiunto la lista dei player con i corrispettivi team

--- LanParty/Assets/c# scripts/playerInfo/New Version Code/LobbyManager.cs
+++ LanParty/Assets/c# scripts/playerInfo/New Version Code/LobbyManager.cs
@@ -8,10 +8,10 @@ using Unity.Netcode;
 public class LobbyManager : NetworkBehaviour
 {
     public static LobbyManager instance {get; private set;}
-    private Dictionary<ulong, bool> playersReadyServerRpcDic;
+    private Dictionary<ulong, UtenteReady> playersReadyServerRpcDic;
     private void Awake(){
         instance = this;
-        playersReadyServerRpcDic = new Dictionary<ulong, bool>();
+        playersReadyServerRpcDic = new Dictionary<ulong, UtenteReady>();
     }
 
     public void SetPlayerReady(){
@@ -20,21 +20,34 @@ public class LobbyManager : NetworkBehaviour
 
     [ServerRpc(RequireOwnership = false)]
     private void SetPlayerReadyServerRpc(ServerRpcParams serverRpcParams = default){
-        playersReadyServerRpcDic[serverRpcParams.Receive.SenderClientId] = true;
-
+        playersReadyServerRpcDic[serverRpcParams.Receive.SenderClientId].ready = true;
+        playersReadyServerRpcDic[serverRpcParams.Receive.SenderClientId]._name = GameObject.Find("UserName").GetComponent<TMP_InputField>().text;
         bool allClientReady = true;
 
         foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds)
         {
-            if(!playersReadyServerRpcDic.ContainsKey(clientId) || !playersReadyServerRpcDic[clientId]){
+            if(!playersReadyServerRpcDic.ContainsKey(clientId) || !playersReadyServerRpcDic[clientId].ready){
                 allClientReady = false;
                 break;
             }
         }
 
         if(allClientReady){
+            GameObject g = Instantiate(new GameObject());
+
+            g.AddComponent<c>();
+            g.GetComponent<c>().players = playersReadyServerRpcDic;
+            DontDestroyOnLoad(g);
             NetworkManager.Singleton.SceneManager.LoadScene("GameSchoolScene", UnityEngine.SceneManagement.LoadSceneMode.Single);
         }
 
     }
+}
+public class UtenteReady{
+    public bool ready;
+    public string _name;
+}
+
+public class c : MonoBehaviour{
+    public  Dictionary<ulong, UtenteReady> players;
 }
