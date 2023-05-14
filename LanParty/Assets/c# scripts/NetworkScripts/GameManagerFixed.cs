bae1eb8615368565709569efc00acf9593350ef2
federicopotrich <federico.potrich@marconirovereto.it>
2023-05-14 16:56:59 +0200

    proseguimento della sistemazione delle telecamere

--- LanParty/Assets/c# scripts/NetworkScripts/GameManagerFixed.cs
+++ LanParty/Assets/c# scripts/NetworkScripts/GameManagerFixed.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using System.Linq;
 
 public class GameManagerFixed : NetworkBehaviour
 {
@@ -15,31 +16,29 @@ public class GameManagerFixed : NetworkBehaviour
     {
         Instance = this;
     }
-    void Update(){
-        if(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "GameSchoolScene" && b == true){
-            if(NetworkManager.Singleton.IsServer){
+    void Update()
+    {
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "GameSchoolScene" && b == true)
+        {
+            if (NetworkManager.Singleton.IsServer)
+            {
                 //GameObject.Find("Camera").SetActive(true);
                 NetworkManager.Singleton.SceneManager.OnLoadEventCompleted += SceneManager_OnLoadEventCompleted;
             }
             b = false;
         }
     }
-    private void SceneManager_OnLoadEventCompleted(string sceneName, UnityEngine.SceneManagement.LoadSceneMode loadSceneMode, List<ulong> clientsCompleted, List<ulong> clientsTimedOut){
-        int ctr = 0;
+    private void SceneManager_OnLoadEventCompleted(string sceneName, UnityEngine.SceneManagement.LoadSceneMode loadSceneMode, List<ulong> clientsCompleted, List<ulong> clientsTimedOut)
+    {
         foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds)
         {
-            if(NetworkManager.IsClient){
-                Transform playerTransform = Instantiate(playerPrefab);
-                Camera playerCamera = playerTransform.GetComponentInChildren<Camera>();
-                playerCamera.gameObject.name = "Camera: " + ctr;
-
-                // Imposta la camera del player come camera principale della scena
-                playerCamera.tag = "MainCamera";
-                playerCamera.enabled = true;
-                playerCamera.targetDisplay = ctr;
-                playerTransform.GetComponent<NetworkObject>().SpawnAsPlayerObject(clientId, true);
-                ctr++;
-            }
+            List<ulong> clientsList = new List<ulong>(clientsCompleted);
+            int clientIndex = clientsList.IndexOf(clientId);
+            GameObject cameraObj = GameObject.Find("Camera: " + clientIndex);
+            // cameraObj.tag = "MainCamera";
+            // cameraObj.GetComponent<Camera>().enabled = true;
+            // cameraObj.GetComponent<AudioListener>().enabled = true; // se vuoi abilitare anche l'AudioListener della telecamera
         }
+
     }
 }
